b83d85747e044a8fbcc4f2093e563e41dd680c28
Rahinur Bin Naushad <rahinur2004@gmail.com>
2025-07-01 19:39:19 +0600

    Forgot to add MOD

--- Competitive Programming/CP_CPP/Number theory/Binary Exponentiation.cpp
+++ Competitive Programming/CP_CPP/Number theory/Binary Exponentiation.cpp
@@ -5,19 +5,19 @@ typedef long long ll;
 
 const int MOD = 1e9 + 7;
 ll binpow(ll a, ll b) {
-    ll res = 1;
-    while (b > 0) {
-        if (b & 1)
-            res = res * a;
-        a = a * a;
-        b >>= 1;
-    }
-    return res;
+  ll res = 1;
+  while (b > 0) {
+    if (b & 1)
+      res = (res * a) % MOD;
+    a = (a * a) % MOD;
+    b >>= 1;
+  }
+  return res;
 }
 
 //================actual code starts from here===========================================
 void solve() {
-    cout << binpow(100, 3);
+  cout << binpow(100, 3);
 }
 
 int main() {
